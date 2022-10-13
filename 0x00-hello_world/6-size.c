ee626e8d85df23bdbb1d816acb8cea8154bff5d8
uausofteng <uausofteng@gmail.com>
2022-10-13 13:42:02 -0700

    t
    sk 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long long d;
 	float e;
 
-	printf("Size of a char: %d byte(s)\n", sizeof(a));
-	printf("Size of an int: %d byte(s)\n", sizeof(b));
-	printf("Size of a long int: %d byte(s)\n", sizeof(c));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
-	printf("size of a float: %d byte(s)\n", sizeof(e));
+	printf("Size of a char: %1d byte(s)\n", sizeof(a));
+	printf("Size of an int: %4d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %4d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %8d byte(s)\n", sizeof(d));
+	printf("size of a float: %4d byte(s)\n", sizeof(e));
 	return (0);
 }
