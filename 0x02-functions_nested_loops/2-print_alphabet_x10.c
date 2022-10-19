19675fb88dbd046ac812619b0d8bafbd2e751bbb
uausofteng <uausofteng@gmail.com>
2022-10-18 18:21:53 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+* prints_alphabet_x10 - prints alpha 10 times
+* Description - print_alphabet_10x
+*/
+void print_alphabet_10x(void)
+{
+	int 10;
+	char la;
+
+	for (ten = 0; ten <= 9; ten++)
+{
+	for (la = 'a'; la = 'z'; la++)
+		_putchar(la);
+	putchar('\n');
+}
+}
