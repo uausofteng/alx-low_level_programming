cca05d5b83227271b9feb7daca458a11fbba077b
uausofteng <uausofteng@gmail.com>
2022-10-20 06:29:11 -0700

    task 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* retrun value of main - main retrun value of main test function
+* main - main return value of main
+* Return 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
