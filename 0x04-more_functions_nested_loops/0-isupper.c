7f1a5a49f2016f515fcfeafd8baf80375301e92d
uausofteng <uausofteng@gmail.com>
2022-10-21 02:27:39 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
-*_isupper - check if a character is or not
-*@c: character to be tested
-*Return: 1 whether it is, 0 otherwise
+* _isupper - check if a character is or not
+* @c: character to be tested
+* Return: 1 whether it is, 0 otherwise
 */
 
 int_isupper(int c)
 {
-if ((c >= 'A') && (c <= 'Z'))
-{
-return (1);
-}
-return (0);
+	if ((c >= 'A')  && (c <= 'Z'))
+	{
+		return (1);
+	}
+
+	return (0);
 }
