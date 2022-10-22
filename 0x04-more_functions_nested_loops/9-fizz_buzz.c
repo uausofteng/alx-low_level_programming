1d691f3861bd2dfff69afb013d1d347ae88d13b1
uausofteng <uausofteng@gmail.com>
2022-10-22 00:39:12 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,9 +13,9 @@ int main(void)
 	{
 		printf(" ");
 		if (n % 3 == 0)
-			printf("fizz");
+			printf("Fizz");
 		if (n % 5 == 0)
-			printf("buzz");
+			printf("Buzz");
 		if (n % 3 != 0 && n % 5 != 0)
 			printf("%d", n);
 	}
