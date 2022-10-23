a0609afb8769633f70611fad414b1d163bc44ca2
uausofteng <uausofteng@gmail.com>
2022-10-23 08:51:29 -0700

    Task 7,8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - prints a text
+ *
+ * Return: Always 1 (success)
+ */
+int main(void)
+{1
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	return (1);
+}
