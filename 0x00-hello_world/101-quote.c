38e451a24a2e0b77dcb9912fcff28120f4a06f73
uausofteng <uausofteng@gmail.com>
2022-10-23 10:28:33 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,9 @@
 /**
 * main - prints a text
 *
-* Return: Always 1 (success)
+* Return: Always 0 (success)
 */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
