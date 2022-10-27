1bed314607aee0a993567ea0a862e9250244cf98
uausofteng <uausofteng@gmail.com>
2022-10-27 05:03:26 -0700

    Task 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,8 +7,9 @@
 * @src: the string to be appended to dest
 * @n: the number of bytes from src appended to dest
 *
-* Return: A pointer to the resulting string dest
+* Return: A pointer to the resulting string des
 */
+
 char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0, dest_len = 0;
@@ -16,7 +17,7 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[index++])
 		dest_len++;
 
-	for (index = 0; src[index] && < n; index++)
+	for (index = 0; src[index] && index  < n; index++)
 		dest[dest_len++] = src[index];
 
 	return (dest);
