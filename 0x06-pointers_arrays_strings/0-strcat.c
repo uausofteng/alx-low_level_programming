dc5d671d1a4ad9e6e05279f3e4a63731da3b046f
uausofteng <uausofteng@gmail.com>
2022-10-27 02:16:39 -0700

    Task 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
-* _strcat - concatenates the sring pointed
-* @dest: pointer to string to be concatenated
+* _strcat - concatenates the sring pointed to by@src, including the terminat
+*    null byte, totheend of string pointed to by@dest.
+* @dest: pointer to string to be concatenated upon.
 * @src: the source string to appended to @dest
-* Return: a pointer to tje destination @dest
+* Return: a pointer to the destination @dest
 */
 char *_strcat(char *dest, const char *src)
 {
-	int index = 0, len_dest = 0;
+	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
