9b1709417c5289077cc9ff3819d6fccb2fb96978
uausofteng <uausofteng@gmail.com>
2022-10-27 06:44:46 -0700

    Task 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -19,7 +19,7 @@ char *_strncpy(char *dest, char *src, int n)
 	for (index = 0; src[index] && index  < n; index++)
 		dest[index] = src[index];
 
-	for (index = 0; src_len; index < n; index++)
+	for (index = src_len; index < n; index++)
 		dest[index] = '\0';
 
 	return (dest);
