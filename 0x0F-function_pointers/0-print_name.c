a806f3e86ef837eff56a440f047b6a0c52fedcfe
uausofteng <uausofteng@gmail.com>
2022-11-16 07:27:18 -0800

    second update for task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,13 @@
+0 - print_name.c
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name.
- * @name: input name.
- * @f: function pointer.
- *
- * Return: no return.
- */
+* print_name - prints a name.
+* @name: input name.
+* @f: function pointer.
+*
+* Return: no return.
+*/
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
