70512fa0819619e84afe8bcfb19a9f6c0e606775
uausofteng <uausofteng@gmail.com>
2022-11-25 02:44:29 -0800

    update task 0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,14 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- *
- * File: main.h
- * Auth: me
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x08-recursion directory
- */  
-
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _printnt _strlen_recursion(char *s);
